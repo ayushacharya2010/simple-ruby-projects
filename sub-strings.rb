3883cf94281749996e457c07e415076a3b625c68
ayushacharya2010 <78182122+ayushacharya2010@users.noreply.github.com>
2024-08-07 11:48:14 -0500

    Add some common 3 and 4 letter words to substrings

--- sub-strings.rb
+++ sub-strings.rb
@@ -1,6 +1,9 @@
 substrings = 
 ["he", "me", "no", "up", "to", "an", "at", "it", "is", "be", 
 "or", "in", "go", "as", "am", "us", "we", "so", "do", "on", 
-"my", "by", "if"]
-
-p substrings
+"my", "by", "if", "the", "and", "but", "can", "not", "all", 
+"one", "you", "his", "her", "its", "out", "are", "for", "was", 
+"who", "why", "how", "any", "our", "new", "now", "that", "with", 
+"from", "have", "this", "they", "will", "your", "know", "what", 
+"when", "them", "then", "many", "some", "like", "just", "good", 
+"time", "take", "come"]
